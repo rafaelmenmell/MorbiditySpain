810674dfd05114f72a19198d7d4e9d44b3c3de7c
Rafael <rafael.menendez@meteologica.com>
2017-11-17 13:51:55 +0100

    Prueba de random forest

--- pruebas3.R
+++ pruebas3.R
@@ -13,6 +13,7 @@ library(lubridate)
 library(ggplot2)
 library(geofacet)
 library(zoo)
+library(randomForest)
 
 if (file.exists("morbi_data.rds")==FALSE){
   ll <- GetMorbiData(y1 = 2005,y2 = 2015)
@@ -21,7 +22,18 @@ if (file.exists("morbi_data.rds")==FALSE){
   ll <- readRDS("morbi_data.rds")
 }
 
-#hay algun ingreso de 2014
-llpeq <- ll %>% filter(year(fecha_ingreso)>=2010)
-
+#en la comunidad de madrid
+llpeq <- ll %>% FilterProvincia(28) %>% filter(year(fecha_ingreso)>=2014)
+rm(ll)
 llpeq <- llpeq %>% AddDiagnosis1() %>% AddDiagnosis2()
+
+llpeq <- llpeq[complete.cases(llpeq),]
+
+set.seed(333)
+N <- nrow(llpeq)
+sample_size <- floor(0.75*N)
+train_ind <- sample(1:N,sample_size)
+train <- llpeq[train_ind,]
+test <- llpeq[-train_ind,]
+
+fit <- randomForest(estancia ~ sexo + diag_in + fecha_ingreso + edad +  diag1 + diag2,data=train,importance=TRUE,ntree=50,nodesize=3,sampsize=sample_size*.3)
